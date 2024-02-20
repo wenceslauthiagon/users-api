7194da247f921b961803be401226e70cd0b59fa5
Thiago Wenceslau <wenceslauncthiago@outlook.com>
2024-02-19 23:42:15 -0300

    feat: add typeOrm Module file.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,24 @@
+
 import { Module } from '@nestjs/common';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
-  controllers: [AppController],
+  imports: [
+    UsersModule,
+    TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: 'root',
+    database: 'test',
+    entities: [ dirname + '/**/*.entity{.ts,.js}',],
+    synchronize: true,
+})],
+controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
